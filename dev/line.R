70ce8b6c80ff7376cddac2d31dae1d7b4a6a55de
Mauricio Vargas <mvargas@dcc.uchile.cl>
2019-08-07 15:07:46 -0400

    adds Ariel to ctb

---
+++ dev/line.R
@@ -0,0 +1,26 @@
+library(d3po)
+library(dplyr)
+
+line_data <- tibble(
+  id = "alpha",
+  ab1 = c(0, -1, 1, 0),
+  ab2 = c(1, 2, 5, 0)
+)
+
+d3po() %>%
+  d3po_type("line") %>%
+  d3po_data(data = line_data) %>%
+  d3po_id("id") %>%
+  d3po_axis(x = "ab2", y = "ab2") %>%
+  d3po_title(
+    list(
+      value = "This is a title",
+      sub = "This is a subtitle"
+    )
+  ) %>%
+  d3po_footer(
+    list(
+      link = "https://www.duckduckgo.com",
+      value = "Click here to search DuckDuckGo"
+    )
+  )

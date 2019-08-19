8ca4d4142dcefb94e4a913b0633d996a24ad4475
Sylvain Lesage <severo@rednegra.net>
2019-08-19 11:21:46 +0200

    feat: add support for new (d3 5.x) categorical color schemes

--- javascript/src/color/schemes.js
+++ javascript/src/color/schemes.js
@@ -3,7 +3,16 @@
 
     var schemes = {},
         dict = {
-            "category10": d3.schemeCategory10
+            "category10": d3.schemeCategory10,
+            "accent": d3.schemeAccent,
+            "dark2": d3.schemeDark2,
+            "paired": d3.schemePaired,
+            "pastel1": d3.schemePastel1,
+            "pastel2": d3.schemePastel2,
+            "set1": d3.schemeSet1,
+            "set2": d3.schemeSet2,
+            "set3": d3.schemeSet3,
+            "tableau10": d3.schemeTableau10
         },
         default = "category10";
 

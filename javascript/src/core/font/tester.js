65d3bdb6d3aab6ae8a6f06b1d21320d2621e27dd
Sylvain Lesage <severo@rednegra.net>
2019-08-20 10:13:57 +0200

    fix: .styles() ans .attrs() functions are not included in d3 bundle

--- javascript/src/core/font/tester.js
+++ javascript/src/core/font/tester.js
@@ -15,7 +15,9 @@
         attrs = type === "div" ? {} : {
             position: "absolute"
         };
-        tester = d3.select("body").selectAll(type + ".d3po_tester").data([0]).join(type).classed("d3po_tester", true).styles(styles).attrs(attrs);
+        tester = d3.select("body").selectAll(type + ".d3po_tester").data([0]).join(type).classed("d3po_tester", true);
+        for (var name in styles) tester.style(name, styles[name]);
+        for (var name in attrs) tester.attr(name, attrs[name]);
         return tester;
     };
 

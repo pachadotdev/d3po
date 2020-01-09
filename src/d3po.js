31c395dbc5f0adbb0e7b0791c37483e54a44b55d
Curran <curran.kelleher@gmail.com>
2020-01-09 11:02:14 +0530

    Fix eslint errors in buckets.js

--- src/d3po.js
+++ src/d3po.js
@@ -23114,9 +23114,7 @@ function close_descriptions() {
 // Expands a min/max into a specified number of buckets
 (function() {
   module.exports = function(arr, n) {
-    var buckets, step;
-    buckets = [];
-    step = (1 / (n - 1)) * (arr[1] - arr[0]);
+    var step = (1 / (n - 1)) * (arr[1] - arr[0]);
     return d3.range(arr[0], arr[1] + step, step);
   };
 }.call(this));

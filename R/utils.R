ce1982171007463ac2818bf226aa90a57862d6cb
JohnCoene <jcoenep@gmail.com>
2020-10-15 19:35:32 +0200

    abstract widget creation for d3po method + fix sizin (using sizingpolicy instead of hardcoded values) + add unique key on preRender hook

--- R/utils.R
+++ R/utils.R
@@ -1,6 +1,52 @@
-# internal function
+#' Get Data
+#' 
+#' Returns the appropriate data.
+#' 
+#' @param x,y Dataframes to choose from.
+#' 
+#' @return Either `x` or `y` is not `NULL`.
+#' 
+#' @noRd
+#' @keywords internal
 .get_data <- function(x, y){
   if(!is.null(y))
     return(y)
   return(x)
+}
+
+#' Build Widget
+#' 
+#' Builds the widget.
+#' 
+#' @inheritParams d3po
+#' 
+#' @noRd 
+#' @keywords internal
+widget_this <- function(x, width = NULL, height = NULL, elementId = NULL){
+  htmlwidgets::createWidget(
+    name = "d3po",
+    x,
+    preRenderHook = .render_d3po,
+    width = width,
+    height = height,
+    package = "d3po",
+    sizingPolicy = htmlwidgets::sizingPolicy(
+      padding = 5,
+      defaultWidth = "100%",
+      defaultHeight = 400L
+    ),
+    elementId = elementId
+  )
+}
+
+# remove tempdata
+# this is important to make sure we don't share
+# sensitive data points => only serialize what the user explicitely wants
+.render_d3po <- function(d3po) {
+  if(!"d3poKey" %in% names(d3po$x$data)){
+    d3po$x$data[["d3poKey"]] <- 1:nrow(d3po$x$data)
+  }
+  d3po$x$tempdata <- NULL
+  d3po$x$daes <- NULL
+  return(d3po)
 }

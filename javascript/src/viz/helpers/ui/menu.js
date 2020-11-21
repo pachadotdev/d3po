2dbcd6ac8db6d974241c8fbd36d81159fcebe72b
fbecerra <becerrafernando@gmail.com>
2020-11-20 22:25:49 -0300

    add menu to save image as png file

---
+++ javascript/src/viz/helpers/ui/menu.js
@@ -0,0 +1,16 @@
+// Create menu to download image
+module.exports = function(type) {
+    var styles, menu;
+    styles = {
+        position: "absolute",
+        right: "10px",
+        top: "10px",
+        display: "block"
+    };
+    menu = d3.select("body").selectAll("div.d3po_menu").data([0]);
+    menu.enter().append("div").attr("class", "d3po_menu").style(styles).text('Save PNG')
+      .on("click", function(){
+        saveSvgAsPng(document.getElementById('d3po'), "diagram.png")
+      });
+    return menu;
+};

2547d2c7fc7529be28da44f84385f34b64246226
fbecerra <becerrafernando@gmail.com>
2021-01-22 09:12:55 -0300

    add button to download as png

--- javascript/src/viz/helpers/ui/menu.js
+++ javascript/src/viz/helpers/ui/menu.js
@@ -3,12 +3,16 @@ module.exports = function(type) {
     var styles, menu;
     styles = {
         position: "absolute",
-        left: "10px",
+        right: "10px",
         top: "10px",
+        // visibility: "hidden",
         display: "block"
     };
     menu = d3.select("body").selectAll("div.d3po_menu").data([0]);
-    menu.enter().append("div").attr("class", "d3po_menu").style(styles).text('Save PNG')
+    menu.enter().append("div").attr("class", "d3po_menu").style(styles)//.text('Save PNG')
+      .append("ion-icon")
+      .attr("name", "download")
+      .style("font-size", "30px")
       .on("click", function(){
         saveSvgAsPng(document.getElementById('d3po'), "diagram.png")
       });
